d4b37378788faed49f899c3eb3417b6f2bc96b2c
johntirazona <johnjenlytirazona@gmail.com>
2026-02-05 16:50:57 +0800

    did my online ordering w04 project

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("123 Smith St.", "Provo", "UT", "USA");
+        Customer customer1 = new Customer("John Smith", address1);
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Notebook", "P001", 2.50, 4));
+        order1.AddProduct(new Product("Pen", "P002", 1.25, 6));
+
+        Address address2 = new Address("10 Bayani Ave.", "Taguig", "Metro Manila", "Philippines");
+        Customer customer2 = new Customer("John Jenly Tirazona", address2);
+        Order order2 = new Order(customer2);
+
+        order2.AddProduct(new Product("Backpack", "P010", 25.00, 1));
+        order2.AddProduct(new Product("Water Bottle", "P011", 10.00, 2));
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order1.GetTotalCost()}\n");
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.GetTotalCost()}");
     }
 }
